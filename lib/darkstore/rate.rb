014498b9b6ffd2fb25397f44db0a669c2e3c4926
Daniele Palombo <daniele.palombo@gmail.com>
2018-02-21 12:12:38 +0100

    Search by sku instead product_id to retrieve shipping rate information
    Darkstore has a product_name parameter that can be used to filter
    shipping rates by product sku. This PR adds that parameter so that it
    can be used

--- lib/darkstore/rate.rb
+++ lib/darkstore/rate.rb
@@ -1,11 +1,12 @@
 module Darkstore
   class Rate < Api
-    def delivery_rates(darkstore_id: nil, zip:, cart:)
+    def delivery_rates(darkstore_id: nil, zip:, cart:, product_name: nil)
       params = {}
 
       params[:darkstore_id] = darkstore_id unless darkstore_id.nil?
       params[:order] = { zip: zip } unless zip.nil?
       params[:cart] = cart unless cart.nil?
+      params[:product_name] = true if product_name == true
 
       request(:get, 'delivery_rates', params: parse_json(params))
     end
